a9a5871edbe9971201c78fec255e28cd5e8969c4
Em <contactemilyhuang@gmail.com>
2022-07-16 05:37:49 -0700

    Add field for account schema

--- models/account.js
+++ models/account.js
@@ -5,6 +5,7 @@ var Schema = mongoose.Schema;
 var accountSchema = new Schema({
 	username: { type: String, required: true },
 	password: { type: String, required: true },
+	isMember: { type: Boolean, default: false },
 	isAdmin: { type: Boolean, default: false }
 });
 
--- models/post.js
+++ models/post.js
@@ -3,9 +3,9 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var postSchema = new Schema({
-	poster: { type: Schema.Types.ObjectId, ref: 'Account', required: true },
-	text: { type: String, required: true },
-	date: { type: Date, required: true }
+	author: { type: Schema.Types.ObjectId, ref: 'Account', required: true },
+	body: { type: String, required: true },
+	date: { type: Date, default: Date.now, required: true }
 });
 
 module.exports = mongoose.model('Post', postSchema);
