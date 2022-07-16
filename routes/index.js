ca488001530afdf1a7336f283e989eb400d7bee3
Em <contactemilyhuang@gmail.com>
2022-07-16 06:49:45 -0700

    Add structure and simple routes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-	res.render('index', { title: 'Express' });
+	res.render('index');
+});
+
+router.get('/sign-up', function (req, res, next) {
+	res.render('signuplogin');
+});
+
+router.get('/login', function (req, res, next) {
+	res.render('signuplogin', { isLogin: true });
 });
 
 module.exports = router;
