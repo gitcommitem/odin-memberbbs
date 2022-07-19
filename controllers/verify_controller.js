34b6cdd08f03def5c6280ff443cf7a09632aecdc
Em <contactemilyhuang@gmail.com>
2022-07-19 08:18:43 -0700

    Add POST method to verify member form

---
+++ controllers/verify_controller.js
@@ -0,0 +1,46 @@
+var Account = require('../models/account');
+
+const { body, validationResult } = require('express-validator');
+const { request } = require('../app');
+
+exports.verify_isMember_post = [
+	// Validate and sanitize fields.
+	body('code', 'Input must not be empty')
+		.trim()
+		.isLength({ min: 1 })
+		.escape(),
+
+	// Process request after validation and sanitization.
+	(req, res, next) => {
+		// Extract the validation errors from a request.
+		const errors = validationResult(req);
+
+		//Change isMember from false to true
+		var account = new Account({
+			username: req.user.username,
+			password: req.user.password,
+			isMember: true,
+			isAdmin: false,
+			_id: req.user.id
+		});
+
+		if (!errors.isEmpty() || req.body.code !== 'github') {
+			// There are errors. Render form again with sanitized values/error messages.
+			const stringError = JSON.stringify(errors.array());
+			const parsedError = JSON.parse(stringError);
+
+			res.redirect('/');
+			return;
+		} else {
+			if (req.body.code === 'github') {
+				Account.findByIdAndUpdate(req.user.id, account, {}, function (err) {
+					if (err) {
+						return next(err);
+					}
+					//Redirect to index upon success
+					res.redirect('/');
+				});
+			}
+		}
+	}
+];
--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ var index_controller = require('../controllers/index_controller');
 var signup_controller = require('../controllers/signup_controller');
 var login_controller = require('../controllers/login_controller');
 var post_controller = require('../controllers/post_controller');
+var verify_controller = require('../controllers/verify_controller');
 
 /* GET home page. */
 router.get('/', index_controller.index);
@@ -30,5 +31,6 @@ router.post('/sign-up', signup_controller.signup_post);
 router.post('/login', login_controller.login_post);
 router.post('/new-post', post_controller.post_new_post);
 router.post('/delete-post', post_controller.post_delete_post);
+router.post('/verify-member', verify_controller.verify_isMember_post);
 
 module.exports = router;
